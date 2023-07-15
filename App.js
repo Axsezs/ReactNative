78b2ebfa74cabe3f971bcd3942d4e84286e5225b
Axsezs <136871352+Axsezs@users.noreply.github.com>
2023-07-15 12:18:39 +0700

    Attendance[15-July-2023]

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Cat from './components/Cat';
+import ViewBoxesWithColorAndText from './components/ViewBoxesWithColorAndText';
+import DisplayandImage from './components/DisplayandImage';
+import LotOfGreeting from './components/LotOfGreeting';
+import MyCustomTextWith from './components/MyCustomTextWith';
+
 export default function App() {
   return (
-    <View>
-      <Cat/>
-    </View>
+    // <View style = {styles.container}>
+    //   {/* <ViewBoxesWithColorAndText/>
+    //   <DisplayandImage/> */}
+    // </View>
+    //<LotOfGreeting/>
+    <MyCustomTextWith/>
   );
+  
 }
+const styles = StyleSheet.create({
+  container:{
+     flex:1,   
+     justifyContent:'center',    
+     alignItems:'center'   
+  }
+ })
 
